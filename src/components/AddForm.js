50176272c40c19216d1609f96e3798f2f91af785
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-30 12:38:07 -0700

    updating AddForm.js

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -8,6 +8,7 @@ const AddForm = (props) => {
     const {addNewSmurf, setError, error} = props
 
     const [state, setState] = useState({
+        id: Date.now(),
         name:"",
         position:"",
         nickname:"",
@@ -23,18 +24,26 @@ const AddForm = (props) => {
 
 // [x] 3. Within the handleSubmit function, replace the static assignment to errorMessage with a call to the setError action. Test that an error is displayed when this validation code fails.
 // [x] 4. Within the handleSubmit function, call your addSmurf action with the smurf name, position, nickname and summury passed as arguments. Test that a smurf is correctly added to when the form is submitted.
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //     if (state.name === "" || state.position === "" || state.nickname === "") {
+    //         setError = "Name, position and nickname fields are required.";
+    //     }
+    //     const newSmurf = {
+    //         name: state.name,
+    //         position: state.position,
+    //         nickname: state.nickname,
+    //         description: state.description
+    //     }
+    //     addNewSmurf(newSmurf);
+    // }
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            setError = "Name, position and nickname fields are required.";
+            setError( "Name, position and nickname fields are required." );
+        }else{
+            addNewSmurf( state )
         }
-        const newSmurf = {
-            name: state.name,
-            position: state.position,
-            nickname: state.nickname,
-            description: state.description
-        }
-        addNewSmurf(newSmurf);
     }
     
 // [x] 2. Replace all instances of the errorMessage static variable with your error message state value. 
