594e0687c1fc5e186d5cad2bfcb81b8e6787fd66
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-30 02:58:58 -0700

    added all SMURF cases in reducers, fetchSmurfs performing axios call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,27 @@
 import axios from 'axios';
 
-export const FETCH_SMURFS = 'FETCH_SMURFS';
+export const START_FETCH_SMURFS = 'START_FETCH_SMURFS';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
 export const ADD_SMURFS = 'ADD_SMURFS';
 export const SMURFS_ERROR = 'SMURFS_ERROR';
 
 // [x] 1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
-// [ ] 2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
-// [ ] 3. Add a standard action that allows us to set the value of the error message slice of state.
-
 export const fetchSmurfs = () => {
     return (dispatch) => {
-        dispatch({type: FETCH_SMURFS})
-        console.log('is this thing on?');
+        dispatch({type: START_FETCH_SMURFS})
+        // console.log('is this thing on?');
+
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then((res)=>{
+                console.log(res, 'SMURFDATA')
+                dispatch({type: FETCH_SMURFS_SUCCESS, payload: res.data});
+            })
+            .catch((err)=>{
+                dispatch({type: FETCH_SMURFS_FAILURE, payload: err.message});
+            })
     }
 }
+// [ ] 2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
+// [ ] 3. Add a standard action that allows us to set the value of the error message slice of state.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
-import {} from '../actions/index';
+import {FETCH_SMURFS, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAILURE, ADD_SMURFS, SMURFS_ERROR, START_FETCH_SMURFS} from '../actions/index';
 
-export const initialState = {
+const initialState = {
     // [x] 1. Adds the following state values into the initialState:
     //  - an array of smurfs
     //  - a boolean indicating if the app is loading
@@ -12,15 +12,41 @@ export const initialState = {
 // [x] 2. Add in the arguments needed to complete a standard reducer function.
 const reducer = (state = initialState, action)=>{
     switch(action.type){
+    // [x] 3. Add in a reducer case to accomidate the start of a smurf fetch.    
+        case (START_FETCH_SMURFS):
+            return({
+                ...state,
+                isLoading: false,
+                error: ''
+            })
+    // [x] 4. Add in a reducer case to accomidate the successful smurf api fetch.
+        case (FETCH_SMURFS_SUCCESS):
+            return({
+                ...state,
+                isLoading: false,
+                error: ''
+            })
+    // [x] 5. Add in a reducer cases to accomidate the failed smurf api fetch.
+        case (FETCH_SMURFS_FAILURE):
+            return({
+                ...state,
+                isLoading: false,
+                error: ''
+            })
+    // [x] 6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
+    case (ADD_SMURFS):
+        return({
+            ...state,
+        })
+    // [x] 7. Add in a reducer case that adds in a value to the error message.    
+    case (SMURFS_ERROR):
+        return({
+            ...state
+        })
+
         default:
             return state;
     }
 }
 
 export default reducer;
-
-// [ ] 3. Add in a reducer case to accomidate the start of a smurf fetch.
-// [ ] 4. Add in a reducer case to accomidate the successful smurf api fetch.
-// [ ] 5. Add in a reducer cases to accomidate the failed smurf api fetch.
-// [ ] 6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-// [ ] 7. Add in a reducer case that adds in a value to the error message.
